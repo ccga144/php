16e01b65c575461cc2e2ffc0bdf9d89044d14856
ccga144 <84418989+ccga144@users.noreply.github.com>
2022-01-01 17:24:14 -0300

    ex001.php

---
+++ exercícios/ex001/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Testando PHP</h1>";
+        echo "<h2>Olá Mundo</h2>";
+        $idade = 3;
+        $sal = 1825.54;
+        $nome =  "Leonardo";
+        $casado = false;
+    ?>
+</body>
+</html>
