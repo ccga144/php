53408708c6987d269f2ff7faceca0183fbd59d4f
ccga144 <84418989+ccga144@users.noreply.github.com>
2022-01-05 09:16:55 -0300

    ex002

---
+++ exercícios/ex002/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, inital-scale=1.0">
+    <title>Document</title>
+</head>
+</html>
